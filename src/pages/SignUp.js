f5e5a2e3d3479057a32bbbfb20f5c1b14c3e2a11
rurariyu <rurariyudayo@gmail.com>
2024-11-28 10:44:31 +0900

    signup notfound

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./../css/SignUp.css";
+import Config from "../Config";
 
 function SignUp() {
     // 初期値
@@ -17,7 +18,7 @@ function SignUp() {
             password:password
         }
         try{
-            const response = await fetch("http://localhost:5000/newUser", {
+            const response = await fetch(`${Config.azureBackUrl}/newUser`, {
                 method:"POST",
                 headers: {
                     "Content-Type": "application/json",
