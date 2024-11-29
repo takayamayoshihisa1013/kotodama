f27df1c9e87c9a98ec52518cf3c61bc29bb1dc85
rurariyu <rurariyudayo@gmail.com>
2024-11-29 10:37:28 +0900

    1129 1

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
 const config = {
-    azureBackUrl:"hatchpostback-h0fecdbvcsepbed4.japaneast-01.azurewebsites.net"
+    azureBackUrl:"https://hatchpostback-h0fecdbvcsepbed4.japaneast-01.azurewebsites.net"
 }
 export default config;
